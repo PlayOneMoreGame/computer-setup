48f30f1dc4c5131b8bb279900a0cec02375466ff
Patrick Wyatt <pat@codeofhonor.com>
2020-02-24 13:13:34 -0800

    Correct bug caused by script rename

--- bin/host-setup.bat
+++ bin/host-setup.bat
@@ -1,3 +1,3 @@
 @echo off
-SET DIR=%~dp0%
-"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command "& '%DIR%host-setup.ps1'"
+SET FNAME=%~dpn0%
+"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command "& '%FNAME%.ps1'"
