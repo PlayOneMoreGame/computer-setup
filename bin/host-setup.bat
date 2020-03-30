464dfcd273698b7a4d08da118790580c573c47fe
Patrick Wyatt <pat@codeofhonor.com>
2020-03-30 09:35:00 -0700

    Update setup scripts to allow arguments to be passed

--- bin/host-setup.bat
+++ bin/host-setup.bat
@@ -1,3 +1,4 @@
 @echo off
 SET FNAME=%~dpn0%
-"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command "& '%FNAME%.ps1'"
+:: Run a Powershell script of the same name as this batch file with "bypass" so it will actually run on vanilla windows
+"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -File "%FNAME%.ps1" %*
