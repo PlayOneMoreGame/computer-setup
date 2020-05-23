890ff78bc2cde99486c7b513f66d15cc670eccc7
Patrick Wyatt <pat@codeofhonor.com>
2020-05-22 18:09:39 -0700

    Bash script removes extension to run batch file of same name

--- bin/win-software-setup.sh
+++ bin/win-software-setup.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Run a Windows batch script of the same name as this bash file
-exec cmd.exe //q //c "$SCRIPT_DIR/$(basename "${BASH_SOURCE[0]}").bat" "$@"
+exec cmd.exe //q //c "$SCRIPT_DIR/$(basename "${BASH_SOURCE[0]%.*}").bat" "$@"
