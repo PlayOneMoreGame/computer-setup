8b4434a96ddb6fdd1e064a694291982e7fcb04f6
Jared Chavez <jared@onemoregame.com>
2021-10-27 17:33:50 -0700

    tweaks

--- bin/intune-bootstrap.ps1
+++ bin/intune-bootstrap.ps1
@@ -1,5 +1,5 @@
 $cli = New-Object System.Net.WebClient
 $cli.CachePolicy = New-Object System.Net.Cache.RequestCachePolicy -ArgumentList NoCacheNoStore 
 
-$cli.DownloadString("https://raw.githubusercontent.com/PlayOneMoreGame/computer-setup/intune/bin/win-software-setup.ps1") | Out-File bootstrap.ps1
-./bootstrap.ps1 -WorkingDir C:\omg\ps
+$cli.DownloadString("https://raw.githubusercontent.com/PlayOneMoreGame/computer-setup/intune/bin/win-software-setup.ps1") | Out-File win-software-setup.ps1
+./win-software-setup.ps1 -WorkingDir C:\omg\ps
